d912575e6df892840dab97a34e85a5504c4fe432
oneofthezombies <hunhoekim@gmail.com>
2024-04-27 16:36:35 +0000

    wip: battle

--- src/server/contents/battle.cc
+++ src/server/contents/battle.cc
@@ -109,5 +109,7 @@ auto contents::Battle::OnStart(const engine::EventLoop &event_loop,
     battle_states_.emplace(second_socket_id, std::move(battle_state));
   }
 
+  event_loop.Write();
+
   return ResultT{Void{}};
 }
