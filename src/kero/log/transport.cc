4cb93951383ea7357a38c1b64dd4ec2d71d00076
oneofthezombies <hunhoekim@gmail.com>
2024-04-30 10:16:34 +0900

    chore: console transport

--- src/kero/log/transport.cc
+++ src/kero/log/transport.cc
@@ -21,14 +21,56 @@ kero::Transport::GetLevel() const noexcept -> Level {
 }
 
 auto
-kero::ConsolePlainTextTransport::OnLog(const Log& log) noexcept -> void {
-  const auto datetime = TimePointToIso8601(std::chrono::system_clock::now());
-  std::cout << datetime << " " << LevelToString(log.level) << " "
-            << log.location.file_name() << ":" << log.location.line() << ":"
-            << log.location.column() << " '" << log.location.function_name()
-            << "' " << log.message;
-  for (const auto& [key, value] : log.data) {
-    std::cout << " " << key << ": " << value;
+PrintIndent(std::ostream& os, const size_t indent) -> void {
+  for (size_t i = 0; i < indent; ++i) {
+    os << " ";
   }
-  std::cout << std::endl;
+}
+
+auto
+kero::ConsolePlainTextTransport::OnLog(const Log& log) noexcept -> void {
+  auto& os = std::cout;
+  size_t indent{0};
+  PrintIndent(os, indent);
+
+  const auto datetime = TimePointToIso8601(std::chrono::system_clock::now());
+  os << datetime;
+  os << " ";
+  os << LevelToString(log.level);
+  os << " - ";
+  os << log.message;
+  os << "\n";
+  indent += 2;
+
+  if (!log.data.empty()) {
+    PrintIndent(os, indent);
+    os << "data:\n";
+    const auto data_indent = indent + 2;
+    for (auto it = log.data.begin(); it != log.data.end(); ++it) {
+      PrintIndent(os, data_indent);
+      os << it->first << ": " << it->second;
+      if (std::next(it) != log.data.end()) {
+        os << "\n";
+      }
+    }
+    os << "\n";
+  }
+
+  if (log.location.file_name() != nullptr) {
+    PrintIndent(os, indent);
+    os << "location: ";
+    os << log.location.file_name();
+    os << ":";
+    os << log.location.line();
+    os << ":";
+    os << log.location.column();
+    os << "\n";
+  }
+
+  if (log.location.function_name() != nullptr) {
+    PrintIndent(os, indent);
+    os << "function: ";
+    os << log.location.function_name();
+    os << "\n";
+  }
 }
